910e1dacd03e81f29c9a64bd622ca81dcb206f4c
MichaelHo <michelo851a1203@gmail.com>
2025-11-09 11:53:07 +0800

    feat(): update issue # 2025-11-09-005
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def life_span(_: FastAPI):
+    logger.info("FastAPI application starting on port 8080...")
+    yield
+
 
 app = FastAPI()
 
@@ -9,3 +20,7 @@ def root():
 @app.get("/health")
 def check_health():
     return { "status": "OK" }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
